be422af7c37f3305bd9577abaa399067d7a2b95f
Federico Sapuppo <fedesapuppo@hotmail.com>
2024-01-17 18:21:46 +0200

    create LineItemDate M, V, association, routes, fixtures and styles. Modify Quotes C

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "quotes#index"
-  resources :quotes
+  root 'quotes#index'
+  resources :quotes do
+    resources :line_item_dates, except: %i[index show]
+  end
 end
